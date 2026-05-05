628f427c8120e0e9d17de7bfcab951f4b4cc3e94
bineshshrestha17 <bineshshrestha17@gmail.com>
2026-05-05 21:20:36 +0545

    calorie_tracker screen added
    calorie_tracker screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 import 'chest_exercise.dart';
+=======
+import 'calorie_tracker_screen.dart';
+>>>>>>> faeb548 (calorie_tracker screen added)
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +15,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+<<<<<<< HEAD
       debugShowCheckedModeBanner: false,
       title: 'Fitness App',
       home: ChestExerciseScreen(), // this will show your chest exercise screen
+=======
+      title: 'FitLog',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CalorieTrackerScreen(),
+>>>>>>> faeb548 (calorie_tracker screen added)
     );
   }
 }
