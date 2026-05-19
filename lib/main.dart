ebfe44872fe7b1d426c4053e0e860a187356ad8a
Rijan <your_email@example.com>
2026-05-19 09:26:08 +0545

    Resolved merge conflict in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,26 @@
+import 'package:fitlog/chest_exercise.dart';
 import 'package:flutter/material.dart';
-import 'chest_exercise.dart';
+import 'change_password_screen.dart';
+import 'workout_tracking_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'FitLog',
       debugShowCheckedModeBanner: false,
-      title: 'Fitness App',
-      home: ChestExerciseScreen(), // this will show your chest exercise screen
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const WorkoutTrackingScreen(),
     );
   }
 }
+
