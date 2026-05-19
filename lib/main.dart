a27387f3c55d312949d842c9da0e1081920d2d7a
Rijan <rijanpradhan666@gmail.com>
2026-05-19 21:48:43 +0545

    Initial commit on safal branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:fitlog/chest_exercise.dart';
 import 'package:flutter/material.dart';
 import 'workout_tracking_screen.dart';
+import 'meal_tracking_screen.dart';
+import 'membership_tracking_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,14 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FitLog',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const WorkoutTrackingScreen(),
+      theme: ThemeData.dark(),
+      home: const MembershipTrackingScreen(),
     );
   }
 }
-
