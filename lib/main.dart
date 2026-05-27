cbf5af0d4dba63132ab0b7ee25582f58b9edf93d
KritikaTripathi094 <kritikatripathi0094@gmail.com>
2026-05-27 22:06:31 +0545

    works

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:fitlog/view/fitlog_elite.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
@@ -34,7 +35,7 @@ class FitLogApp extends StatelessWidget {
         ),
       ),
 
-      home: FitLogApp(),
+      home: FitlogEliteApp(),
     );
   }
 }
