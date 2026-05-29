5b0252b696424b8d3b95736f67dde45cbc6a957e
kshitiz-eyyy <pokharelkshitiz5@gmail.com>
2026-05-29 10:14:47 +0545

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-
-// Teammate's imports
-import 'package:fitlog/bmi_calculator_screen.dart';
-import 'package:fitlog/fitlog_login.dart';
-import 'package:fitlog/forgot_password_screen.dart';
-import 'package:fitlog/register_screen.dart';
-import 'package:fitlog/workout_timer_screen.dart';
-
-import 'library.dart';
-import 'activity_screen.dart';
-import 'favourite_exercise.dart';
-import 'change_password_screen.dart';
-import 'dashboard.dart';
-import 'features_screen.dart';
-import 'splash_screen.dart';
-=======
->>>>>>> 27c451ffebb4a1b0af647655be65a845dab755a9
-
 import 'firebase_options.dart';
 import 'view/library.dart';
 import 'view/activity_screen.dart';
