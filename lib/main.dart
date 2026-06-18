4b16d01e51aed84e8dfff005fc7ba55ce4195958
kshitiz-eyyy <pokharelkshitiz5@gmail.com>
2026-06-18 07:07:48 +0545

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fitlog/view/admin_panel_screen.dart';
-
 import 'package:fitlog/view/contact_dietitan_screen.dart';
 import 'package:fitlog/view/contact_trainer_screen.dart';
 import 'package:fitlog/view/fitlog_login.dart';
@@ -9,27 +8,18 @@ import 'package:fitlog/view/terms_and_conditions_screen.dart';
 import 'package:fitlog/view/user_dashboard.dart';
 import 'package:fitlog/view/welcome_screen.dart';
 import 'package:fitlog/viewmodel/user_view_model.dart';
-
 import 'package:fitlog/view/meal_tracking_screen.dart';
 import 'package:fitlog/view/user_profile.dart';
 import 'package:provider/provider.dart';
 import 'view/fitlog_login.dart';
 import 'package:fitlog/view/testing_gateway_screen.dart;.dart';
-
 import 'package:flutter/material.dart';
-
-
-
-
-
 import 'firebase_options.dart';
 import 'view/library.dart';
 import 'view/favourite_exercise.dart';
 import 'view/change_password_screen.dart';
 import 'view/features_screen.dart';
 import 'view/splash_screen.dart';
-
-
 import 'view/fitlog_premium_screen.dart';
 import 'view/workout_tracking_screen.dart';
 
@@ -69,7 +59,7 @@ class FitLogApp extends StatelessWidget {
             ),
           ),
         ),
-        home:const DashboardScreen()
+        home:const FitLogLogin()
     );
   }
 }
